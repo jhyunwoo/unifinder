3da581b042c5291175f149f87f45227e0d6fd151
jhyunwoo <jhyunwoo0228@gmail.com>
2024-08-14 15:44:09 +0900

    add will be support university on home page

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,9 @@ export default function HomePage() {
         </Link>
       </div>
       <div>지원 대학: 서울대, 연세대, 고려대, 서강대</div>
+      <div>
+        지원 예정 대학: 성균관대, 한양대, KAIST, GIST, UNIST, DGIST, KENTEC
+      </div>
       <div className="text-sm font-semibold text-red-500">
         주의: 베타버전이므로 데이터가 정확하지 않을 수 있습니다. 각 대학별
         수시모집요강을 꼭 확인하시길 바랍니다.
