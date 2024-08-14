17d45de260f273f676b6793c3ec21fd28a6c98e1
jhyunwoo <jhyunwoo0228@gmail.com>
2024-08-14 14:17:12 +0900

    add og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,10 @@ import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "유니파인더 UNIFINDER",
-  description: "2025학년도 수시 지원 카드 6장 검색",
+  description: "2025학년도 대학교 입시 대비 수시 지원 카드 검색",
+  metadataBase: new URL(
+    process.env.SITE_URL ? process.env.SITE_URL : "http://localhost:3000",
+  ),
 };
 
 export default function RootLayout({
